875be510535b7f534ff05370ec65155cb38b4e63
Sam Habiel <sam.habiel@vistaexpertise.net>
2014-02-25 18:16:36 -0500

    routine A1AEUT1:
    2014 02 25: Rick Marshall of the VISTA Expertise Network (VEN/TOAD)
    edited MKSTREAM to use the new standardized name for patch stream OSEHRA
    VISTA, set field Abbreviation (.05) to OV, to change field 2 with field
    .02, comment with field names, and change from index PRIM to APRIM. In
    PATCHVER, fix bug by replacing hardcoded 1 with DA.
    
    routine A1AEUTL:
    
    2014 02 10-11,25: Rick Marshall of the VISTA Expertise Network
    (VEN/TOAD): added change history, new subroutine ID11005 to replace file
    DHCP Patches (11005)'s WRITE identifier logic, changed $$PRIMSTRM to use
    new APRIM index instead of old PRIM index to avoid letting users select
    DHCP Patch Stream records by typing YES or NO. Changed name of default
    record from "VA PATCH STREAM" to "FOIA VISTA" and set its field
    Abbreviation (.05) to "FV". Fixed an intermediate-calculation bug in the
    WRITE ID logic - if patch had status of cancel, it calculated as
    "cancel^0" instead of "cancel" before extracting just the first three
    characters; the results were correct, but the intermediate value was
    wrong. Convert ID11005^A1AEUTL from write commands to use of EN^DDIOL;
    delimit components in silent mode.

--- Routines/A1AEUTL.m
+++ Routines/A1AEUTL.m
@@ -1,5 +1,42 @@
-A1AEUTL ; RMO,MJK/ALBANY ;2014-01-22  7:02 PM
- ;;2.3;Patch Module;;Oct 17, 2007;Build 8
+A1AEUTL ;RMO,MJK/ALBANY,VEN/SMH&TOAD - Patch Utilities ; 2/25/14 5:41pm
+ ;;2.4;Patch Module;;Oct 17, 2007;Build 8
+ ;
+ ; Change History:
+ ;
+ ; 2007 01 11: previous change; by Wally Fort of the Oakland VA Field
+ ; Office (ISF/RWF).
+ ;
+ ; 2014 01 06: Sam Habiel of the VISTA Expertise Network (VEN/SMH):
+ ; in SETNUM, DIC(0) will be taken from the symbol table if it is
+ ; defined to make the output silent for Unit Testing. Otherwise, it
+ ; will default to LE.
+ ;
+ ; 2014 01 10: (VEN/SMH) replace awkward logic in SETNUM based on
+ ; traditional simple index AB on file DHCP Patches (11005) to be
+ ; based on replacement new-style compound index AB. Old algorithm
+ ; was hard-coded to a limit of 999 as the highest patch number.
+ ; New algorithm uses reverse $order to get last patch number, with
+ ; no hard-coded limit.
+ ;
+ ; 2014 01 22: (VEN/SMH) introduce new variable A1AESTRM and logic
+ ; to set initial sequence number and patch number based on new file
+ ; DHCP Patch Stream (11007.1)'s field Patch Number Start (.001).
+ ; Changes in SEQ, NUM, SETNUM, & $$PRIMSTRM.
+ ;
+ ; 2014 02 10-11,25: Rick Marshall of the VISTA Expertise Network
+ ; (VEN/TOAD): added change history, new subroutine ID11005 to
+ ; replace file DHCP Patches (11005)'s WRITE identifier logic,
+ ; changed $$PRIMSTRM to use new APRIM index instead of old PRIM
+ ; index to avoid letting users select DHCP Patch Stream records
+ ; by typing YES or NO. Changed name of default record from
+ ; "VA PATCH STREAM" to "FOIA VISTA" and set its field Abbreviation
+ ; (.05) to "FV". Fixed an intermediate-calculation bug in the WRITE
+ ; ID logic - if patch had status of cancel, it calculated as
+ ; "cancel^0" instead of "cancel" before extracting just the first
+ ; three characters; the results were correct, but the intermediate
+ ; value was wrong.
+ ;
+ ;
  ;logic to get and set seq#
  ; VEN/SMH - Stream logic here -- done, second trail.
 SEQ L +^A1AE(11007,A1AEPKIF,"V",A1AEVR,"PR"):60
@@ -97,17 +134,114 @@ NEWVER(PKIEN,PCHIEN) ;Setup a new version of package.  Called when a Package is 
  K IEN D UPDATE^DIE("","FDA","IEN")
  Q
  ;
+ ;
 PRIMSTRM() ; Return the Primary Stream for this FORUM Patch Module config
- ; Output: Primary Stream IEN.
- ; If the Primary VA entry doesn't exist, add it.
+ ;;private;function;clean;silent;SAC-compliant
+ ; called by: SEQ, NUM
+ ; calls: IX1^DIK
+ ; input: index PRIM of file DHCP Patch Stream (11007.1)
+ ; output = field Patch Number Start (.001/IEN) of primary stream
+ ;   if file contains no records, initialize FOIA VISTA record
+ ;
+ ; if no records, add FOIA VISTA
  I '$D(^A1AE(11007.1,1,0)) D
- . S ^(0)="VA PATCH STREAM^0" ; set
- . N DA,DIK S DA=1,DIK="^A1AE(11007.1," D IX1^DIK ; index
+ . S ^(0)="FOIA VISTA^0^^^FV" ; set Name, Primary?, & Abbreviation
+ . N DA,DIK S DA=1,DIK="^A1AE(11007.1," D IX1^DIK ; cross-reference
  ;
- ; Get primary stream number using PRIM xref
- ; If not found, site is unconfigured. Use 1m+1 instead as a default.
- N PSN S PSN=$O(^A1AE(11007.1,"PRIM",1,""))
- IF 'PSN Q 10**6+1
- ELSE  Q PSN
+ ; get primary stream number using APRIM index
+ N PSN S PSN=$O(^A1AE(11007.1,"APRIM",1,"")) ; primary stream #
+ ; if not found, site is unconfigured
+ I 'PSN S PSN=10**6+1 ; default to 1,000,001
  ;
- ; /PRIMSTRM
+ QUIT PSN ; return primary stream #; end of $$PRIMSTRM
+ ;
+ ;
+ID11005 ; WRITE Identifier on DHCP Patches file (11005)
+ ;;private;procedure;clean;output;SAC-compliant
+ ; called by: ^DD(11005,0,"ID","WRITE")
+ ; calls: none
+ ; input:
+ ;   $X
+ ;   file DHCP Patches (11005), current record:
+ ;     field Patch Stream (.2)
+ ;       :file DHCP Patch Stream (11007.1)
+ ;       :field Abbreviation (.05)
+ ;     field Patch Subject (5)
+ ;     field Status of Patch (8)
+ ;       :DD definition of set of codes
+ ;     field User Entering (9)
+ ;       :file New Person (200)
+ ;       :field Initial (1)
+ ; output to current device (definition):
+ ;   IEN  Designation  [Stream]Subject  Status  User
+ ; output in silent mode:
+ ;   [Stream]Subject|Status|User
+ ;
+ ; Fields IEN (.001) and Patch Designation (.01) are provided by File
+ ; Manager and are not part of this identifier. Everything after that
+ ; - the spaces, field Abbreviation (.05) of file DHCP Patch Stream
+ ; (11007.1) record pointed to by field Patch Stream (.2), truncated
+ ; field Patch Subject (5), abbreviated field Status of Patch (8), and
+ ; abbreviated field Initial (1) of file New Person (200) record of
+ ; field User Entering (9) - are output as part of this write
+ ; identifier. If this Patch Module supports only a single patch stream
+ ; then field .2 will be empty, so it will be omitted from this ID.
+ ;
+ ; output to current device (sample):
+ ;
+ ; Select DHCP PATCHES PATCH DESIGNATION: ??
+ ;
+ ;   Choose from:
+ ;   12           TIU*1*246    [FV]TESTING TESTING               UND TOA
+ ;   13           TIU*1*10002  [OV]TEST                          UND TOA
+ ;   14           ZZZ*2*10001  [OV]TEST                          VER USP
+ ;
+ N ID S ID="" ; initialize identifier
+ N X S X=$X ; current X position
+ N IDLEN S IDLEN=80-X ; maximum room for identifier
+ N DELIM S DELIM=" " ; write ID component delimiter, default to space
+ I $G(DIQUIET) S DELIM="|" ; | delim in silent mode
+ ;
+ N PATCH S PATCH=^(0) ; DHCP Patches record's header
+ ;
+ ; Stream:
+ N STREAMDA S STREAMDA=$P(PATCH,U,20) ; field Patch Stream (.2)
+ N STREAM S STREAM=$G(^A1AE(11007.1,+STREAMDA,0)) ; record hdr
+ S STRABB=$P(STREAM,U,5) ; field Abbreviation (.05)
+ I STRABB'="" D  ; skip stream if empty
+ . S ID="["_STRABB_"]" ; add Stream to ID
+ . S IDLEN=IDLEN-$L(ID) ; decrease room
+ ;
+ ; Subject:
+ N SUBJECT S SUBJECT=$P(PATCH,U,5) ; field Patch Subject (5)
+ N SUBJABB S SUBJABB=$E(SUBJECT,1,IDLEN-10) ; make it fit
+ S ID=ID_SUBJABB_DELIM ; add Subject to ID
+ S IDLEN=IDLEN-$L(SUBJABB) ; decrease room
+ ;
+ ; padding:
+ N PAD S $P(PAD," ",IDLEN-8)="" ; create pad
+ I '$G(DIQUIET) S ID=ID_PAD ; add pad to ID
+ ;
+ ; Status:
+ N STATUS S STATUS=$P(PATCH,U,8) ; field Status of Patch (8)
+ N DDSTATUS S DDSTATUS=^DD(11005,8,0) ; definition of field 8, header
+ N DDSET S DDSET=$P(DDSTATUS,U,3) ; definition of set of codes
+ N STATCODE S STATCODE=$P($P(DDSET,STATUS_":",2),";") ; external val
+ S ID=ID_$E(STATCODE,1,3)_DELIM ; add Status to ID
+ ;
+ ; User:
+ N USERENTR S USERENTR=+$P(PATCH,U,9) ; field User Entering (9)
+ N USER S USER=$G(^VA(200,USERENTR,0)) ; New Person record's header
+ N INITIAL S INITIAL=$P(USER,U,2) ; field Initial (1) of file 200
+ I INITIAL="" S INITIAL="unknown" ; if no user or no initials
+ S ID=ID_$E(INITIAL,1,3) ; add User to ID
+ ;
+ ; output write ID:
+ N TAB S TAB=$S(X<19:18,1:X-1) ; tab for terminal
+ I $G(DIQUIET) S TAB=0 ; don't tab for GUI
+ D EN^DDIOL(ID,"","?"_TAB) ; output the write ID
+ ;
+ QUIT  ; end of ID11005
+ ;
+ ;
+EOR ; end of routine A1AEUTL
