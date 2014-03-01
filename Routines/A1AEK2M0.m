4c34d05a92c4020d50570bce0fb582488b0579f2
Sam Habiel <sam.habiel@vistaexpertise.net>
2014-02-28 19:25:52 -0500

    Continuation of A1AEK2M

---
+++ Routines/A1AEK2M0.m
@@ -0,0 +1,241 @@
+A1AEK2M0 ; VEN/SMH - A1AEK2M Continuation;2014-02-28  12:19 PM
+ ;;2.4;DHCP PATCH MODULE;;
+ ;
+ ; Conversion procedure from a VA PM HFS-extracted KIDS (complete):
+ ;^TMP(28177,1,0)="Released TIU*1*241 SEQ #237" <-- $TXT prepended
+ ;^TMP(28177,2,0)="Extracted from mail message" <-- this becomes the txt
+ ;^TMP(28177,3,0)="**KIDS**:TIU*1.0*241^"       <-- $END TXT replaced
+ ;^TMP(28177,4,0)="" --> becomes $KID append whatever is in 6
+ ;^TMP(28177,5,0)="**INSTALL NAME**"
+ ;^TMP(28177,6,0)="TIU*1.0*241"
+ ;---
+ ;^TMP(28177,1189,0)="**END**" --> becomes $END KID whatever is in 6
+ ;^TMP(28177,1190,0)="**END**" --> DELETED
+ ;
+ ; A few random notes on various KIDS issues
+ ; If the original is a mail message, it will looks like this
+ ; 
+ ; >> Released GMRA*4*44 SEQ #41
+ ; >> Extracted from mail message
+ ; >> **KIDS**:GMRA*4.0*44^
+ ; >> 
+ ; >> **INSTALL NAME** etc..
+ ; >> kids contents
+ ; >> **END**
+ ; >> **END**
+ ;
+ ; If the original isn't a PM HFS-extracted KIDS build, but a Straight from 
+ ; KIDS KIDS-build, then the KIDS first line looks like this:
+ ;
+ ; >> KIDS Distribution saved on Apr 30, 2013@05:31:47
+ ; >> OR*371
+ ; >> **KIDS**:OR*3.0*371^
+ ; >> <blank line>
+ ; >> **INSTALL NAME**
+ ;
+ ; Multibuilds look like this:
+ ; 
+ ; >> KIDS Distribution saved on Sep 23, 2011@17:42:57
+ ; >> IB/PRCA Remedy Ticket Fixes
+ ; >> **KIDS**:IB*2.0*459^PRCA*4.5*280^
+ ; >> <blank line>
+ ; >> **INSTALL NAME**
+ ; >> text of first KIDS build
+ ; >> **INSTALL NAME**
+ ; >> text of second KIDS build
+ ; >> **END**
+ ; >> **END**
+ ;
+ ; A KIDS sent from another system via KIDS/MM has the following contents.
+ ;$TXT Created by TESTMASTER,USER at VEN.SMH101.COM  (KIDS) on Thursday, 01/07/14 at 15:55
+ ; <contents>
+ ;$END TXT
+ ;$KID ZZZ*1.0*1
+ ;**INSTALL NAME**
+ ; <contents>
+ ;$END KID ZZZ*1.0*1
+ ;
+KIDFIL(ROOT,PATCH,TXTINFO,KIDGLO) ; $$; Private; Find the KIDS file that corresponds to a patch designation
+ ; ROOT: Ref, File system roots (MP = Multibuild folder)
+ ; PATCH: Val, Text file name
+ ; TXTINFO: Ref, the analyzed Text array
+ ; KIDGLO: Name, the Global into which to load the KIDS contents in PM format.
+ ;
+ ; This code is pretty iterative. It keeps trying different things until it finds the patch.
+ ;
+ N NOEXT S NOEXT=$P(PATCH,".",1,$L(PATCH,".")-1) ; no extension name
+ N KIDFIL0 ; Trial iteration variable
+ N DONE ; Loop exit
+ ;
+ ; Try by file name!
+ N % F %="KID","kid","KIDS","kids","KIDs","kidS" D  Q:$G(DONE)
+ . S KIDFIL0=NOEXT_"."_%
+ . N POP
+ . D OPEN^%ZISH("KID0",ROOT("SB"),KIDFIL0,"R")
+ . I POP S KIDFIL0="" QUIT
+ . D CLOSE^%ZISH("KID0")
+ . ;
+ . ; Okay. At this point we confirmed that the file exists. Is it right though?
+ . K ^TMP($J,"TKID"),^("ANKID") ; Temp KID; Analysis KID
+ . N % S %=$$FTG^%ZISH(ROOT("SB"),KIDFIL0,$NA(^TMP($J,"TKID",1,0)),3)   ; To Global
+ . I '% S $EC=",U-FILE-DISAPPEARED,"
+ . D ANALYZE^A1AEK2M1($NA(^TMP($J,"ANKID")),$NA(^TMP($J,"TKID"))) ; Analyze the file
+ . ;
+ . ; Now, make sure that the TXT file's designation is the same as the KIDS' patch no.
+ . ; Loop through every patch in the file and make sure at least one matches.
+ . N P S P=""
+ . F  S P=$O(^TMP($J,"ANKID",P)) Q:P=""  I $$K2PMD^A1AEK2M(P)=TXTINFO("DESIGNATION") S DONE=1 QUIT
+ . I $G(DONE) DO  QUIT
+ . . M @KIDGLO=^TMP($J,"ANKID",P)
+ . . D EN^DDIOL("Found patch "_TXTINFO("DESIGNATION")_" in "_KIDFIL0)
+ ;
+ ; If we don't have it, get all KIDS files and grab any one that has the
+ ; patch number in its name.
+ I $G(KIDFIL0)="" D  ; Still we don't have it.
+ . N A S A("*.kid")="",A("*.KID")=""  ; Search for these files
+ . S A("*.kid?")="",A("*.KID?")=""    ; and these too; but not the .json ones.
+ . N FILES  ; rtn array by name
+ . N % S %=$$LIST^%ZISH(ROOT("SB"),$NA(A),$NA(FILES)) ; ls
+ . ; I '% S $EC=",U-DIRECTORY-DISAPPEARED," ; should never happen; WRONG: It's a possibility.
+ . I '% QUIT  ; Try the multibuild directory next
+ . K %,A ; bye
+ . ;
+ . N F S F="" ; file looper
+ . N DONE ; control flag
+ . ; here's the core search for the file name containing a patch number
+ . F  S F=$O(FILES(F)) Q:F=""  I F[$P(TXTINFO("DESIGNATION"),"*",3) D  Q:$G(DONE)
+ . . K ^TMP($J,"TKID"),^("ANKID") ; Temp KID; Analysis KID
+ . . N % S %=$$FTG^%ZISH(ROOT("SB"),F,$NA(^TMP($J,"TKID",1,0)),3)   ; To Global
+ . . I '% S $EC=",U-FILE-DISAPPEARED,"
+ . . D ANALYZE^A1AEK2M1($NA(^TMP($J,"ANKID")),$NA(^TMP($J,"TKID"))) ; Analyze the file
+ . . ;
+ . . ; Now, make sure that the TXT file's designation is the same as the KIDS' patch no.
+ . . ; Loop through every patch in the file and make sure at least one matches.
+ . . N P S P=""
+ . . F  S P=$O(^TMP($J,"ANKID",P)) Q:P=""  I $$K2PMD^A1AEK2M(P)=TXTINFO("DESIGNATION") S DONE=1 QUIT
+ . . I $G(DONE) DO  QUIT
+ . . . M @KIDGLO=^TMP($J,"ANKID",P)
+ . . . D EN^DDIOL("Found patch "_TXTINFO("DESIGNATION")_" in "_F)
+ . . . S KIDFIL0=F
+ ;
+ ; Now we have the hard case. We still don't have the file. 
+ ; Let's look in the Multibuilds directory
+ I $G(KIDFIL0)="" D
+ . ; Set-up XTMP
+ . ; NB: NO LOCKS B/C IT'S OKAY FOR MULTIPLE USERS TO FILE THIS SIMULTANEOUSLY
+ . ; NB (CONT): THERE ARE NO COUNTERS WHICH NEED TO BE SYNCHRONIZED.
+ . N XTMPS S XTMPS=$T(+0)
+ . N START S START=$$NOW^XLFDT()
+ . N PURGDT S PURGDT=$$FMADD^XLFDT(START,30)
+ . S ^XTMP(XTMPS,0)=PURGDT_U_START_U_"Analyzed Multibuilds Holding Area"
+ . ;
+ . ; Load the Multibuild file names
+ . N A S A("*.kid")="",A("*.KID")=""  ; Search for these files
+ . S A("*.kid?")="",A("*.KID?")=""    ; and these too; but not the .json ones.
+ . N FILES  ; rtn array by name
+ . N % S %=$$LIST^%ZISH(ROOT("MB"),$NA(A),$NA(FILES)) ; ls
+ . I '% S $EC=",U-DIRECTORY-DISAPPEARED," ; should never happen
+ . K %,A ; bye
+ . ;
+ . N F S F="" ; file looper
+ . N DONE ; control flag
+ . ; Analyze each Multibuild
+ . F  S F=$O(FILES(F)) Q:F=""  D  Q:$G(DONE)
+ . . D EN^DDIOL("Analyzing Multibuild file "_F) ; print out
+ . . I '$D(^XTMP(XTMPS,F)) D  ; If it isn't loaded already...
+ . . . K ^TMP($J,"TKID"),^("ANKID") ; Temp KID; Analysis KID
+ . . . N % S %=$$FTG^%ZISH(ROOT("MB"),F,$NA(^TMP($J,"TKID",1,0)),3)   ; To Global
+ . . . I '% S $EC=",U-FILE-DISAPPEARED,"
+ . . . D ANALYZE^A1AEK2M1($NA(^TMP($J,"ANKID")),$NA(^TMP($J,"TKID"))) ; Analyze the file
+ . . . M ^XTMP(XTMPS,F)=^TMP($J,"ANKID") ; Put into XTMP
+ . . ; Now, make sure that the TXT file's designation is the same as the KIDS' patch no.
+ . . ; Loop through every patch in the file and make sure at least one matches.
+ . . N P S P=""
+ . . F  S P=$O(^XTMP(XTMPS,F,P)) Q:P=""  I $$K2PMD^A1AEK2M(P)=TXTINFO("DESIGNATION") S DONE=1 QUIT
+ . . I $G(DONE) D  QUIT
+ . . . M @KIDGLO=^XTMP(XTMPS,F,P)
+ . . . D EN^DDIOL("Found patch "_TXTINFO("DESIGNATION")_" in "_F)
+ . . . S KIDFIL0=F
+ ;
+ ; If we still can't find it. Oh well! Can't do nuthin.
+ K ^TMP($J,"TKID"),^("ANKID")
+ QUIT $G(KIDFIL0)
+ ;
+GETSTRM(DESIGNATION) ; Private to package; $$; Get the Stream for a designation using a patch number
+ ; Input: DESIGNATION XXX*1.0*5
+ ; Output: Stream IEN in 11007.1
+ N PN
+ I $L(DESIGNATION,"*")>1 S PN=$P(DESIGNATION,"*",3)
+ E  S PN=0
+ I PN=0 QUIT 1  ; VA Patch Stream
+ N STRM
+ N I F I=0:0 S I=$O(^A1AE(11007.1,I)) Q:'I  D  Q:$G(STRM)
+ . N MIN S MIN=I
+ . N MAX S MAX=I+998
+ . I PN'<MIN&(PN'>MAX) S STRM=I  ; Really this is IF MIN<=PN<=MAX...
+ Q STRM
+ ;
+SELFILQ ; Public; Interactive entry point... ; TODO
+ ; This code is a NO-OP right now.
+ ; I probably would use it in the future, but not now.
+ ; ZEXCEPT: ROOT,PATCH
+ N KIDFIL
+ N ARRAY S ARRAY("*.KI*")="",ARRAY("*.ki*")=""
+ N FILE
+ N Y S Y=$$LIST^%ZISH(ROOT("SB"),$NA(ARRAY),$NA(FILE))
+ I 'Y  ; TODO!!! -- probably ask the user to try again since directory has no KIDS files.
+ S KIDFIL=$$SELFIL(.FILE,,"Select a KIDS build to match to "_PATCH)
+ QUIT KIDFIL
+ ;
+SELFIL(FILES,EXTFILTER,DIRA) ; Public; INTERACTIVE ; Select a file from a list
+ ; FILES = Ref List of files from LIST^%ZISH
+ ; EXTFILTER = Val .TXT or so
+ ; DIRA = Val What to ask the user for
+ ; Uses fileman calls to ease the pain of selecting stuff.
+ ;
+ N I S I=""
+ ; Filter away using the extension
+ I $L($G(EXTFILTER)) F  S I=$O(FILES(I)) Q:I=""  D
+ . I $E($RE(I),1,$L(EXTFILTER))'=$RE(EXTFILTER) K FILES(I)
+ ;
+ ; If no files left, quit with an empty string
+ Q:'$L($O(FILES(""))) ""
+ ;
+ ; Create a global for DIR/DIC
+ K ^TMP($J,"FILES")
+ S ^TMP($J,"FILES",0)="File List"
+ N CNT S CNT=1
+ F  S I=$O(FILES(I)) Q:I=""  S ^TMP($J,"FILES",CNT,0)=I,CNT=CNT+1
+ ;
+ ; Index
+ N DIK,DA S DIK="^TMP($J,""FILES""," D IXALL^DIK
+ ; Select
+ N DIR,X,Y,DIROUT,DIRUT,DTOUT,DUOUT,DIROUT
+ S DIR(0)="P^TMP($J,""FILES"",",DIR("A")=$G(DIRA,"Select a file from the list") D ^DIR
+ ; Bye
+ K ^TMP($J,"FILES")
+ ;
+ I $L(Y,U)=2 Q $P(Y,U,2)
+ E  QUIT ""
+ ;
+CLEANHF(MSGGLO) ; Private... Clean header and footer in message global
+ ; WARNING - Naked all over inside the do block.
+ N S S S=$O(@MSGGLO@("")) ; first numeric sub.
+ I @MSGGLO@(S,0)'["$TXT Created by " D
+ . ; First line is invalid. Try various patterns.
+ . N I F I=1:1 N PATT S PATT=$T(CLNPATT+I),PATT=$P(PATT,";;",2) Q:($$TRIM^XLFSTR(PATT)=">>END<<")  D
+ . . I $$TRIM^XLFSTR(^(0))=$$TRIM^XLFSTR(PATT) S ^(0)="$TXT Created by UNKNOWN,UNKNOWN at DOWNLOADS.VA.GOV  (KIDS)"
+ . ; If still not there, put in first node before the message.
+ . I ^(0)'["$TXT Created by " S @MSGGLO@(S-1,0)="$TXT Created by UNKNOWN,UNKNOWN at DOWNLOADS.VA.GOV  (KIDS)"
+ ;
+ N LASTSUB S LASTSUB=$O(@MSGGLO@(" "),-1)
+ I @MSGGLO@(LASTSUB,0)'["$END TXT" S @MSGGLO@(LASTSUB+1,0)="$END TXT"
+ QUIT
+ ;
+CLNPATT ;; Headers to substitute if present using a contains operator. 1st one is just a blank -- INTENTIONAL
+ ;;
+ ;;*********************
+ ;;Original message:
+ ;;This informational patch
+ ;;>>END<<
+ ;
