ba7f67a72dc723b0eb25636f105029e221d35cb0
Sam Habiel <sam.habiel@vistaexpertise.net>
2014-03-28 16:16:32 -0400

    TM now uses Standard FM date output

--- Routines/A1AEUTL1.m
+++ Routines/A1AEUTL1.m
@@ -1,4 +1,4 @@
-A1AEUTL1	; RMO,MJK/ALBANY ;1/11/07  12:01
+A1AEUTL1	; RMO,MJK/ALBANY ;2014-03-27  1:54 PM
 	;;2.3;Patch Module;;Oct 17, 2007;Build 8
 	;
 PCHK	;call if $d(^(d0,"q","b")), return az(), k az,print
@@ -56,5 +56,5 @@ DATE	;Ask Date Range of Verified patches
 	W ! S %DT="APEX",%DT("A")="   Ending    DATE : " D ^%DT K %DT S:Y<0 POP=1 Q:Y<0  W ! S ENDDATE=Y
 	Q
 XTM	S Y=$E(Y,4,5)_"/"_$E(Y,6,7)_"/"_$E(Y,2,3)_$P("@"_$E(Y_0,9,10)_":"_$E(Y_"000",11,12),"^",Y[".") S:Y="//" Y="" Q
-TM	S Y=$E(Y,4,5)_"/"_$E(Y,6,7)_"/"_$E(Y,2,3) S:Y="//" Y="" Q
+TM	X ^DD("DD") Q  ; Expect Y and output Y.
 	Q
