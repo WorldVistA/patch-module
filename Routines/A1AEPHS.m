2c33481b05ae4a3d61fe0c9f9cd30332b7ef220e
Sam Habiel <sam.habiel@vistaexpertise.net>
2014-04-18 01:46:44 +0000

    Added PHS1 and moved some code there from PHS to reduced routine size

--- Routines/A1AEPHS.m
+++ Routines/A1AEPHS.m
@@ -1,5 +1,5 @@
-A1AEPHS ; RMO,MJK/ALBANY - Logic from DD, U triggers test Message ; 3/31/14 6:49pm
- ;;2.3;Patch Module;;Oct 17, 2007;Build 8
+A1AEPHS ; RMO,MJK/ALBANY - Logic from DD, U triggers test Message ;2014-04-18  12:56 AM
+ ;;2.4;PATCH MODULE;;APR 17, 2007;Build 8
  ;
  ; Input Transform of field Status (8) of file DHCP Patches (11005)
  ;
@@ -524,32 +524,7 @@ BUL ; send bulletin
  ;
  ;
 ESSMSG ; Send message to Remedy to let it know that a patch has been
- ; released or entered-in-error
- N XMTEXT,XMINSTR,STAT,PID,PRI,REL,COMP,I,CAT,%
- S PID=$P(A1AE0,U,1)
- I $P(PID,"*",2)=999 D
- . S $P(PID,"*",2)="DBA" ; Patch ID
- I $L($G(A1AEX))>2 D
- . S PID=A1AEX ;rwf
- S REL=$$FMTE^XLFDT(DT,"5Z") ; Release Date
- I A1AENEW="e" D
- . S STAT=2 ; Entered-in-Error
- . S (PRI,COMP,CAT)=""
- E  D
- . S STAT=3 ; Released
- . S COMP=$$FMTE^XLFDT($P(A1AE0,U,18),"5Z") ; Install by Date
- . S PRI=$P(A1AE0,U,7) ; Priority
- . S I=0
- . S CAT=""
- . F  S I=$O(^A1AE(11005,+$G(DA),"C",I)) Q:'I  D
- . . S CAT=CAT_","_^(I,0) ; Category
- . S CAT=$E(CAT,2,999)
- S XMTEXT(1)=STAT_$$LJ^XLFSTR(PID,30)_$$LJ^XLFSTR("",30)_$$LJ^XLFSTR(REL,22)_$$LJ^XLFSTR(COMP,22)_PRI_$$LJ^XLFSTR(CAT,23)
- S XMINSTR("FROM")="POSTMASTER"
- D SENDMSG^XMXAPI(DUZ,"NPM/ESS Transaction","XMTEXT","G.A1AE PACKAGE RELEASE@FORUM.OSEHRA.ORG",.XMINSTR) ; VEN/SMH - changed.
- ;
- QUIT  ; end of ESSMSG
- ;
+ G ESSMSG^A1AEPHS1
  ;
 ASKTST Q:'$D(^A1AE(11005.1,DA,0))
  W !!,?3,"Option to create a Patch message to send to test sites."
